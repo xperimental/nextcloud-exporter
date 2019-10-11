99772fd55b0ad22f3bcf953a8157cd599a4df430
Robert Jacob <xperimental@solidproject.de>
2019-10-11 21:10:16 +0200

    Remove PasswordFile field.

--- config.go
+++ config.go
@@ -6,27 +6,26 @@ import (
 	"io/ioutil"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/spf13/pflag"
 )
 
 type config struct {
-	ListenAddr   string
-	Timeout      time.Duration
-	InfoURL      *url.URL
-	Username     string
-	Password     string
-	PasswordFile string
+	ListenAddr string
+	Timeout    time.Duration
+	InfoURL    *url.URL
+	Username   string
+	Password   string
 }
 
 func parseConfig() (config, error) {
 	result := config{
-		ListenAddr:   ":9205",
-		Timeout:      5 * time.Second,
-		Username:     os.Getenv("NEXTCLOUD_USERNAME"),
-		Password:     os.Getenv("NEXTCLOUD_PASSWORD"),
-		PasswordFile: os.Getenv("NEXTCLOUD_PASSWORD_FILE"),
+		ListenAddr: ":9205",
+		Timeout:    5 * time.Second,
+		Username:   os.Getenv("NEXTCLOUD_USERNAME"),
+		Password:   os.Getenv("NEXTCLOUD_PASSWORD"),
 	}
 
 	rawURL := os.Getenv("NEXTCLOUD_SERVERINFO_URL");
@@ -35,7 +34,6 @@ func parseConfig() (config, error) {
 	pflag.StringVarP(&rawURL, "url", "l", rawURL, "URL to nextcloud serverinfo page.")
 	pflag.StringVarP(&result.Username, "username", "u", result.Username, "Username for connecting to nextcloud.")
 	pflag.StringVarP(&result.Password, "password", "p", result.Password, "Password for connecting to nextcloud.")
-	pflag.StringVar(&result.PasswordFile, "password-file", result.PasswordFile, "File containing the password for connecting to nextcloud.")
 	pflag.Parse()
 
 	if len(rawURL) == 0 {
@@ -52,8 +50,9 @@ func parseConfig() (config, error) {
 		return result, errors.New("need to provide a username")
 	}
 
-	if len(result.PasswordFile) != 0 {
-		password, err := readPasswordFile(result.PasswordFile)
+	if strings.HasPrefix(result.Password, "@") {
+		fileName := strings.TrimPrefix(result.Password, "@")
+		password, err := readPasswordFile(fileName)
 		if err != nil {
 			return result, fmt.Errorf("can not read password file: %s", err)
 		}
