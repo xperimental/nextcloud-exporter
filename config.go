8dc13e31f66b99e4406faf5bb15eb2b5238c79d1
Robert Jacob <xperimental@solidproject.de>
2019-10-11 22:07:45 +0200

    Load configuration from file.

--- config.go
+++ config.go
@@ -5,14 +5,23 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/url"
+	"os"
 	"strings"
 	"time"
 
 	"github.com/spf13/pflag"
+	"gopkg.in/yaml.v2"
 )
 
 const (
 	defaultPath = "/ocs/v2.php/apps/serverinfo/api/v1/info"
+
+	envPrefix        = "NEXTCLOUD_"
+	envListenAddress = envPrefix + "LISTEN_ADDRESS"
+	envTimeout       = envPrefix + "TIMEOUT"
+	envInfoURL       = envPrefix + "SERVERINFO_URL"
+	envUsername      = envPrefix + "USERNAME"
+	envPassword      = envPrefix + "PASSWORD"
 )
 
 type config struct {
@@ -32,10 +41,25 @@ type rawConfig struct {
 }
 
 func parseConfig() (config, error) {
-	raw := loadConfigFromFlags()
+	raw, configFile := loadConfigFromFlags()
+
+	if configFile != "" {
+		rawFile, err := loadConfigFromFile(configFile)
+		if err != nil {
+			return config{}, fmt.Errorf("error reading configuration file: %s", err)
+		}
+
+		raw = mergeConfig(raw, rawFile)
+	}
+
+	env, err := loadConfigFromEnv()
+	if err != nil {
+		return config{}, fmt.Errorf("error reading environment variables: %s", err)
+	}
+	raw = mergeConfig(raw, env)
 
 	if len(raw.InfoURL) == 0 {
-		return config{}, errors.New("need to provide an info URL")
+		return config{}, errors.New("need to set an info URL")
 	}
 
 	result, err := convertConfig(raw)
@@ -61,16 +85,17 @@ func defaultConfig() config {
 	}
 }
 
-func loadConfigFromFlags() (result rawConfig) {
+func loadConfigFromFlags() (result rawConfig, configFile string) {
 	defaults := defaultConfig()
+	pflag.StringVarP(&configFile, "config-file", "c", "", "Path to YAML configuration file.")
 	pflag.StringVarP(&result.ListenAddr, "addr", "a", defaults.ListenAddr, "Address to listen on for connections.")
 	pflag.DurationVarP(&result.Timeout, "timeout", "t", defaults.Timeout, "Timeout for getting server info document.")
-	pflag.StringVarP(&result.InfoURL, "url", "l", "", "URL to nextcloud serverinfo page.")
-	pflag.StringVarP(&result.Username, "username", "u", defaults.Username, "Username for connecting to nextcloud.")
-	pflag.StringVarP(&result.Password, "password", "p", defaults.Password, "Password for connecting to nextcloud.")
+	pflag.StringVarP(&result.InfoURL, "url", "l", "", "URL to Nextcloud serverinfo page.")
+	pflag.StringVarP(&result.Username, "username", "u", defaults.Username, "Username for connecting to Nextcloud.")
+	pflag.StringVarP(&result.Password, "password", "p", defaults.Password, "Password for connecting to Nextcloud.")
 	pflag.Parse()
 
-	return result
+	return result, configFile
 }
 
 func convertConfig(raw rawConfig) (config, error) {
@@ -102,6 +127,65 @@ func convertConfig(raw rawConfig) (config, error) {
 	}
 
 	return result, nil
+}
+
+func loadConfigFromFile(fileName string) (rawConfig, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return rawConfig{}, err
+	}
+
+	var result rawConfig
+	if err := yaml.NewDecoder(file).Decode(&result); err != nil {
+		return rawConfig{}, err
+	}
+
+	return result, nil
+}
+
+func loadConfigFromEnv() (rawConfig, error) {
+	result := rawConfig{
+		ListenAddr: os.Getenv(envListenAddress),
+		InfoURL:    os.Getenv(envInfoURL),
+		Username:   os.Getenv(envUsername),
+		Password:   os.Getenv(envPassword),
+	}
+
+	if raw, ok := os.LookupEnv(envTimeout); ok {
+		value, err := time.ParseDuration(raw)
+		if err != nil {
+			return rawConfig{}, err
+		}
+
+		result.Timeout = value
+	}
+
+	return result, nil
+}
+
+func mergeConfig(base, override rawConfig) rawConfig {
+	result := base
+	if override.ListenAddr != "" {
+		result.ListenAddr = override.ListenAddr
+	}
+
+	if override.InfoURL != "" {
+		result.InfoURL = override.InfoURL
+	}
+
+	if override.Username != "" {
+		result.Username = override.Username
+	}
+
+	if override.Password != "" {
+		result.Password = override.Password
+	}
+
+	if override.Timeout != 0 {
+		result.Timeout = override.Timeout
+	}
+
+	return result
 }
 
 func readPasswordFile(fileName string) (string, error) {
