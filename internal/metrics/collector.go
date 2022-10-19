604cf072cca7c90294fdad56491e325f668803fc
Robert Jacob <xperimental@solidproject.de>
2022-10-19 23:21:48 +0200

    Add metrics for email and room shares

--- internal/metrics/collector.go
+++ internal/metrics/collector.go
@@ -257,6 +257,8 @@ func collectShares(ch chan<- prometheus.Metric, shares serverinfo.Shares) error 
 	values["group"] = float64(shares.SharesGroups)
 	values["authlink"] = float64(shares.SharesLink - shares.SharesLinkNoPassword)
 	values["link"] = float64(shares.SharesLink)
+	values["mail"] = float64(shares.SharesMail)
+	values["root"] = float64(shares.SharesRoom)
 
 	return collectMap(ch, sharesDesc, values)
 }
