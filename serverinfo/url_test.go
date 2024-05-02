21b8d5ea2b74acc469e02e8c4b3b97d25520c551
Lukas Schreiner <dev@lschreiner.de>
2024-05-02 17:18:33 +0200

    Include Nextcloud update availability information
    Fix test suite.

--- serverinfo/url_test.go
+++ serverinfo/url_test.go
@@ -13,34 +13,41 @@ func TestInfoURL(t *testing.T) {
 		wantURL    string
 	}{
 		{
-			desc:      "do not skip apps",
+			desc:      "do not skip apps and do not skip update (implicit)",
 			serverURL: "https://nextcloud.example.com",
-			wantURL:   "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=false",
+			wantURL:   "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=false&skipUpdate=false",
 		},
 		{
 			desc:      "skip apps",
 			serverURL: "https://nextcloud.example.com",
 			skipApps:  true,
-			wantURL:   "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=true",
+			wantURL:   "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=true&skipUpdate=false",
 		},
 		{
 			desc:       "do not skip update",
 			serverURL:  "https://nextcloud.example.com",
 			skipUpdate: false,
-			wantURL:    "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipUpdate=false",
+			wantURL:    "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=false&skipUpdate=false",
 		},
 		{
 			desc:       "skip update",
 			serverURL:  "https://nextcloud.example.com",
 			skipUpdate: true,
-			wantURL:    "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipUpdate=true",
+			wantURL:    "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=false&skipUpdate=true",
 		},
 		{
-			desc:       "do not skip update and do not skip apps",
+			desc:       "do not skip update and do not skip apps (explicit)",
 			serverURL:  "https://nextcloud.example.com",
 			skipApps:   false,
 			skipUpdate: false,
-			wantURL:    "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipUpdate=false&skipApps=false",
+			wantURL:    "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=false&skipUpdate=false",
+		},
+		{
+			desc:       "skip update and skip apps",
+			serverURL:  "https://nextcloud.example.com",
+			skipApps:   true,
+			skipUpdate: true,
+			wantURL:    "https://nextcloud.example.com/ocs/v2.php/apps/serverinfo/api/v1/info?format=json&skipApps=true&skipUpdate=true",
 		},
 	}
 
